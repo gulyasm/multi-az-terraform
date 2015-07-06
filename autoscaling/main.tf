5466e167230a9631a25be437ae39d51893e89064
gulyasm <mgulyas86@gmail.com>
2015-07-06 09:32:31 +0200

    Removed unused zones variable.

--- autoscaling/main.tf
+++ autoscaling/main.tf
@@ -1,14 +1,6 @@
 variable "access_key" {}
 variable "secret_key" {}
 
-variable "zones" {
-    default = {
-        zone0 = "us-east-1a"
-        zone1 = "us-east-1e"
-        zone2 = "us-east-1c"
-    }
-}
-
 provider "aws" {
     access_key = "${var.access_key}"
     secret_key = "${var.secret_key}"
